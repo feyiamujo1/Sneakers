31fce4b5d6d03d8a070e710e252277d904e87c17
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2021-10-29 20:36:40 +0100

    Added more features and fixed the shopping cart

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,6 @@ function changeImage(n){
     thumbnail[n-1].className += " selected_thumbnail";
     switch(n){
         case 1:
-
             document.getElementById("display_image").src='images/image-product-1.jpg';
             break;
         case 2:
@@ -86,7 +85,47 @@ function showsidebar(){
 /* Function for placing order */
 var Orderplaced = 0;
 function placeOrder(){
-  var currentOrder = document.getElementById("product_quantity").textContent;
-  Orderplaced = parseInt(Orderplaced) + parseInt(currentOrder);
-  document.getElementById("order_number").innerHTML = Orderplaced;
+  var currentOrder = parseInt(document.getElementById("product_quantity").textContent);
+  if (currentOrder != 0){
+    Orderplaced = parseInt(Orderplaced) + parseInt(currentOrder);
+    document.getElementById("order_number").innerHTML = Orderplaced;
+    document.getElementById("amount_of_product").innerHTML = Orderplaced;
+    document.getElementById("total_cost").innerHTML = parseFloat(125.00 * Orderplaced).toFixed(2);
+    checkQuantity();
+  }
+}
+
+/*This function checks if the amount is set otherwise it will show Your cart is empty. */
+function checkQuantity(){
+  if (Orderplaced<1){
+    document.getElementById("inner_dialog").style.display = "none";
+    document.getElementById("checkout_btn").style.display = "none";
+    document.getElementById("empty_notice").style.display = "block";
+  }else{
+    document.getElementById("inner_dialog").style.display = "flex";
+    document.getElementById("checkout_btn").style.display = "block";
+    document.getElementById("empty_notice").style.display = "none";
+  }
+}
+
+var showdialog = false;
+/*function to show / hide the checkout dialog */
+function toggle_dialog(){
+  checkQuantity()
+  var checkout_dialog = document.getElementById("checkout_dialog");
+  if (showdialog==false){
+    checkout_dialog.style.display = "block";
+    showdialog = true;
+  }
+  else{
+    checkout_dialog.style.display = "none";
+    showdialog = false;
+  }
+}
+
+/*delete order function */
+function delete_order(){
+  Orderplaced = parseInt(0);
+  checkQuantity();
+  document.getElementById("order_number").innerHTML = "";
 }
